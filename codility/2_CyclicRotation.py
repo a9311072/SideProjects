f58d03c291245cdcbf5108b0b5a0752f4788c4a8
a9311072 <a9311072@outlook.com>
2020-09-15 00:41:12 +0800

    Update 2_CyclicRotation.py

--- codility/2_CyclicRotation.py
+++ codility/2_CyclicRotation.py
@@ -40,8 +40,6 @@ In your solution, focus on correctness. The performance of your solution will no
 def solution(A, K):
     if len(A) == 0: return A
     K =  K % len(A) 
-    print(A[-K:])
-    print(A[:-K])
     return A[-K:] + A[:-K]
 
 print(solution([3, 8, 9, 7, 6], 3))
